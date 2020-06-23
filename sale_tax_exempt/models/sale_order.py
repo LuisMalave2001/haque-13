c112a469cb6d7675d834fd59b5ee8cea53a57744
rainierking <rainierking@gitlab.com>
2020-06-23 21:59:27 +0800

    Update formula for checking tax exemption to consider year forecast

--- sale_tax_exempt/models/sale_order.py
+++ sale_tax_exempt/models/sale_order.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 from odoo import api, fields, models
 
 class SaleOrder(models.Model):
@@ -8,7 +11,20 @@ class SaleOrder(models.Model):
     @api.constrains("amount_untaxed")
     def check_so_tax_exempt(self):
         for sale in self:
-            if sale.company_id.so_tax_exempt and sale.amount_untaxed <= sale.company_id.so_tax_exempt_amount:
+            today = fields.Date.context_today(self)
+            fiscalyear_end = today + relativedelta(month=int(sale.company_id.fiscalyear_last_month), day=sale.company_id.fiscalyear_last_day)
+            fiscalyear_start = fiscalyear_end + relativedelta(years=-1, days=1)
+            covered_invoices = self.env["account.move"].search([
+                ("partner_id","=",sale.partner_id.id),
+                ("type","=","out_invoice"),
+                ("state","in",["draft","posted"]),
+                ("invoice_date",">=",fiscalyear_start),
+                ("invoice_date","<=",fiscalyear_end),
+            ])
+            total_invoiced_amount = sum(covered_invoices.mapped("amount_untaxed_signed"))
+            remaining_months = (fiscalyear_end.year - today.year) * 12 + (fiscalyear_end.month - today.month + 1)
+            forecasted_amount = total_invoiced_amount + remaining_months * sale.amount_untaxed
+            if sale.company_id.so_tax_exempt and forecasted_amount <= sale.company_id.so_tax_exempt_amount:
                 for line in sale.order_line:
                     line.tax_id = [(5, 0 ,0)]
                 sale._amount_all()
