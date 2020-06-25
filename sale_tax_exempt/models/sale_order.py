cee86d054c00b6304cd1b9798a5507b07b27e1ce
rainierking <rainierking@gitlab.com>
2020-06-25 21:16:01 +0800

    Use invoice date when checking SO tax exemption

--- sale_tax_exempt/models/sale_order.py
+++ sale_tax_exempt/models/sale_order.py
@@ -8,11 +8,15 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.constrains("amount_untaxed")
+    @api.constrains("amount_untaxed", "invoice_date")
     def check_so_tax_exempt(self):
         for sale in self:
-            today = fields.Date.context_today(self)
-            fiscalyear_end = today + relativedelta(month=int(sale.company_id.fiscalyear_last_month), day=sale.company_id.fiscalyear_last_day)
+            invoice_date = sale.invoice_date and sale.invoice_date.date() or fields.Date.context_today(self)
+            fiscalyear_last_month = int(sale.company_id.fiscalyear_last_month)
+            fiscalyear_last_day = sale.company_id.fiscalyear_last_day
+            fiscalyear_end = invoice_date + relativedelta(month=fiscalyear_last_month, day=fiscalyear_last_day)
+            if invoice_date.month > fiscalyear_last_month:
+                fiscalyear_end += relativedelta(years=1)
             fiscalyear_start = fiscalyear_end + relativedelta(years=-1, days=1)
             covered_invoices = self.env["account.move"].search([
                 ("partner_id","=",sale.partner_id.id),
@@ -22,7 +26,7 @@ class SaleOrder(models.Model):
                 ("invoice_date","<=",fiscalyear_end),
             ])
             total_invoiced_amount = sum(covered_invoices.mapped("amount_untaxed_signed"))
-            remaining_months = (fiscalyear_end.year - today.year) * 12 + (fiscalyear_end.month - today.month + 1)
+            remaining_months = (fiscalyear_end.year - invoice_date.year) * 12 + (fiscalyear_end.month - invoice_date.month + 1)
             forecasted_amount = total_invoiced_amount + remaining_months * sale.amount_untaxed
             if sale.company_id.so_tax_exempt and forecasted_amount <= sale.company_id.so_tax_exempt_amount:
                 amount_untaxed = 0.0
