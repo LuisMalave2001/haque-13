8b9e53ce47bc5ae4f8ea39aefa5bda5cea469e12
rainierking <rainierking@gitlab.com>
2020-09-07 14:24:14 +0800

    [IMP] pakistan_wht: Add xlsx report for vendor tax

--- pakistan_wht/reports/__init__.py
+++ pakistan_wht/reports/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from . import pakistan_tax_report
+from . import res_partner_report_tax
--- pakistan_wht/wizards/__init__.py
+++ pakistan_wht/wizards/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from . import pakistan_tax_report_wizard
+from . import res_partner_report_tax_wizard
