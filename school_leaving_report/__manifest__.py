55dfa2f69fac71b7e8fd2e5c63a5ae66a387d82d
rainierking <rainierking@gitlab.com>
2020-09-18 21:03:25 +0800

    [ADD] school_leaving_report

---
+++ school_leaving_report/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "School Leaving Report",
+
+    'summary': """ School Leaving Report """,
+
+    'description': """ School Leaving Report """,
+
+    'author': "Eduwebgroup",
+    'website': "http://www.Eduwebgroup.com",
+
+    'category': 'Extra Tools',
+    'version': '1.0',
+
+    'depends': [
+        'school_finance',
+    ],
+
+    'data': [
+        'views/res_config_settings_views.xml',
+        'reports/res_partner_report_templates.xml',
+        'reports/res_partner_reports.xml',
+    ],
+}
